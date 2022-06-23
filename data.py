1fc2cb10cbcec93265fd771b84e919e194f89935
lukemcdermottt <lmcdermo@ucsd.edu>
2022-06-22 22:04:16 -0700

    final commit for the day

--- data.py
+++ data.py
@@ -1,28 +1,36 @@
 import numpy as np 
 import pandas as pd
 
-def get_data(path):
+def get_data():
     #Load Dataframes
-    bin_df = pd.read_hdf('/Users/lukemcdermott/Desktop/Physics/Star-Lab/spectral_templates_data_version_june20.h5', key = '/binaries')
-    sin_df = pd.read_hdf('/Users/lukemcdermott/Desktop/Physics/Star-Lab/spectral_templates_data_version_june20.h5', key = '/singles')
+    bin_df = pd.read_hdf('/Users/lukemcdermott/Desktop/Physics/spectral_templates_data_version_june20.h5', key = '/binaries')
+    sin_df = pd.read_hdf('/Users/lukemcdermott/Desktop/Physics/spectral_templates_data_version_june20.h5', key = '/singles')
     #wav_df = pd.read_hdf('/Users/lukemcdermott/Desktop/Physics/spectral_templates_data_version_june20.h5', key = '/wavegrid')
 
-    #Prep data for NN with labels: bin ex = [flux][0], sin ex = [flux][1]
-    bin_data = np.array([bin_df.iloc[:,:441].to_numpy(), np.zeros(bin_df.shape[0])])
-    sin_data = np.array([sin_df.iloc[:,:441].to_numpy(), np.ones(sin_df.shape[0])])
+    #Prep data for NN
+    bin_data = bin_df.iloc[:,:441].to_numpy() #np.zeros(bin_df.shape[0])])
+    sin_data = sin_df.iloc[:,:441].to_numpy() #np.zeros(bin_df.shape[0])])
 
     data = bin_data
+    print(type(data))
+    labels = np.zeros(bin_data.size)
     for _ in range (90):
         data = np.concatenate((data, sin_data), axis = 0)
-
-    #data = [inputs, targets]
-    #inputs = [[flux arr], [flux arr], ...,[flux arr]]
-    #targets = [1, 0, ..., 1]
+        labels = np.concatenate((labels, np.ones(sin_data.size)), axis = 0)
+    
 
     #shuffle data
-    shuffler = np.random.permutation(len(data[0]))
-    shuffled_data = np.array([ data[0][shuffler], data[1][shuffler] ])
+    arr = np.arange(10)
+    np.random.shuffle(arr)
+
+    print(np.shape(data))
+    #use length of data -> i think data.size might be wrong above
+    shuffler = np.arange(20)
+    np.random.shuffle(shuffler)
+    print(shuffler)
+    shuffled_data = data[shuffler] #, labels[shuffler] ])
     
+    print('data done')
     return shuffled_data
 
 
